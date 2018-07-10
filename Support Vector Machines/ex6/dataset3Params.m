415e97ec057a22c49a54d95015c3df971983a81c
Soren Nelson <sorennelson33@gmail.com>
2018-07-10 17:32:59 -0600

    Complete Course

---
+++ Support Vector Machines/ex6/dataset3Params.m
@@ -0,0 +1,52 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+lowest_error = 1000;
+
+for i = 1 : length(vals)
+		for j = 1 : length(vals)
+				test_C = vals(i);
+				test_sigma = vals(j);
+				
+				% Train the model
+				model= svmTrain(X, y, test_C, @(x1, x2) gaussianKernel(x1, x2, test_sigma)); 
+				
+				% Use model to make predictions on CV Set
+				predictions = svmPredict(model, Xval);
+				
+				% Calculate your predictions error vs the actual value of the CV Set
+				pred_error = mean(double(predictions ~= yval));
+				
+				if pred_error < lowest_error
+					lowest_error = pred_error;
+					C = test_C;
+					sigma = test_sigma;
+				end
+				
+		end
+% =========================================================================
+
+end
